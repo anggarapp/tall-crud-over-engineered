d065c613f037a15919900a91a1ea3792b66d1671
Anggara Permana Putra <anggaraperpe@gmail.com>
2023-02-27 05:31:17 +0700

    init: initial project and welcome page

---
+++ resources/views/base.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@yield('title')</title>
+    @livewireStyles
+    @vite('resources/css/app.css')
+    @vite('resources/js/app.js')
+</head>
+
+<body class="antialiased">
+    @yield('sidebar')
+    @yield('body')
+    @yield('script')
+    @livewireScripts
+</body>
+
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,32 @@
+@extends('base')
+@section('title')
+Welcome To Over Engineered CRUD with TALL Stack
+@endsection
+
+@section('body')
+<div
+    class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+    <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+        <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
+            <div class="ml-12">
+                <div class="mt-2 text-gray-600 dark:text-gray-400 text-xl">
+                    Hello Kiddos!
+                </div>
+            </div>
+        </div>
+        <div class="flex justify-center mt-4 sm:items-center sm:justify-between">
+            <div class="text-center text-sm text-gray-500 sm:text-left">
+                <div class="flex items-center">
+
+                </div>
+            </div>
+
+            <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
+                Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+@section('script')
+@endsection
