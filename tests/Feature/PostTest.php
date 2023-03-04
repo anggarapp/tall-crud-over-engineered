b0b3c0ca512317b1a5e1562b76f5dfb37820f8ea
Anggara Permana Putra <anggaraperpe@gmail.com>
2023-03-05 06:56:59 +0700

    update: Test Post

--- tests/Feature/PostTest.php
+++ tests/Feature/PostTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Post;
 use App\Services\PostService;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -43,5 +44,90 @@ class PostTest extends TestCase
         $collection = $this->postService->getAllPosts();
         self::assertInstanceOf(Collection::class, $collection);
         $this->assertNotNull($collection->toQuery()->get());
+    }
+
+    public function testGetPostById(): void
+    {
+
+        $this->postService->createPost([
+            'title' => 'Post By Id',
+            'content' => 'Post By Id Test Case'
+        ]);
+
+        $this->assertDatabaseHas('posts', [
+            'title' => 'Post By Id',
+            'content' => 'Post By Id Test Case'
+        ]);
+
+        $post = Post::where('title', 'Post By Id')->first();
+        $postId = $post->id;
+        $postFromService = $this->postService->getPostById($postId);
+        $postIdFromService = $postFromService->id;
+        $this->assertSame($postId, $postIdFromService);
+    }
+
+    public function testCreatePost(): void
+    {
+        $this->postService->createPost([
+            'title' => 'Post Create',
+            'content' => 'Post Create Test Case'
+        ]);
+
+        $this->assertDatabaseHas('posts', [
+            'title' => 'Post Create',
+            'content' => 'Post Create Test Case'
+        ]);
+    }
+
+    public function testUpdatePost(): void
+    {
+        $this->postService->createPost([
+            'title' => 'Post Create',
+            'content' => 'Post Update Test Case'
+        ]);
+
+        $this->assertDatabaseHas('posts', [
+            'title' => 'Post Create',
+            'content' => 'Post Update Test Case'
+        ]);
+
+        $postId = Post::where('title', 'Post Create')->first()->id;
+
+        $this->postService->updatePost($postId, [
+            'title' => 'Post Update',
+            'content' => 'Post Updated Test Case'
+        ]);
+
+        $this->assertDatabaseHas('posts', [
+            'title' => 'Post Update',
+            'content' => 'Post Updated Test Case'
+        ]);
+
+        $this->assertDatabaseMissing('posts', [
+            'title' => 'Post Create',
+            'content' => 'Post Update Test Case'
+        ]);
+    }
+
+    public function testDeletePost(): void
+    {
+        $this->postService->createPost([
+            'title' => 'Post Create',
+            'content' => 'Post Delete Test Case'
+        ]);
+
+        $this->assertDatabaseHas('posts', [
+            'title' => 'Post Create',
+            'content' => 'Post Delete Test Case'
+        ]);
+
+        $postId = Post::where('title', 'Post Create')->first()->id;
+
+        $this->postService->deletePost($postId);
+
+        $this->assertDatabaseMissing('posts', [
+            'title' => 'Post Create',
+            'content' => 'Post Delete Test Case'
+        ]);
     }
 }
